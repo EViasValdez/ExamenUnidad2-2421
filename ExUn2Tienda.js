22eaa45fcdb9655967111d55f3112236c13f50a0
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2021-11-08 14:40:10 -0600

    Examen en equipo

---
+++ ExUn2Tienda.js
@@ -0,0 +1,62 @@
+var total = [];
+////////////////
+//Aqui van las funciones//
+function agrecarro1(){
+    var botonElement = document.getElementById("boton1");
+    var pElement = document.getElementById("uno");
+    var contador = 0;
+    botonElement.onclick = function () {
+        contador++;
+        pElement.textContent = contador;
+        var los3 = parseInt(document.getElementById('numero1').value);
+        total.push(los3);
+    }
+}
+function agrecarro2(){
+    var botonElement = document.getElementById("boton2");
+    var pElement = document.getElementById("uno");
+    var contador = 0;
+    botonElement.onclick = function () {
+        contador++;
+        pElement.textContent = contador;
+        var los3 = parseInt(document.getElementById('numero2').value);
+        total.push(los3);
+    }
+}
+function agrecarro3(){
+    var botonElement = document.getElementById("boton3");
+    var pElement = document.getElementById("dos");
+    var contador = 0;
+    botonElement.onclick = function () {
+        contador++;
+        pElement.textContent = contador;
+        var los3 = parseInt(document.getElementById('numero3').value);
+        total.push(los3);
+    }
+}
+function agrecarro4(){
+    var botonElement = document.getElementById("boton4");
+    var pElement = document.getElementById("dos");
+    var contador = 0;
+    botonElement.onclick = function () {
+        contador++;
+        pElement.textContent = contador;
+        var los3 = parseInt(document.getElementById('numero4').value);
+        total.push(los3);
+    }
+}
+function calcular(){
+    let uno = document.getElementById("uno");
+    let monitorValue = parseInt(uno.innerHTML);
+
+    let dos = document.getElementById("dos");
+    var dosValue = parseInt(dos.innerHTML);
+    
+    let sum = 0;
+
+    for (let index = 0; index < total.length; index++) {
+        sum += total[index];
+    }
+
+    document.getElementById("resultado").value = sum;
+}
