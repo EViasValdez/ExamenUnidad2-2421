a7da3183440fd3a514fb359af5a7f41fdd55a004
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2025-04-14 20:33:04 -0600

    Update ExUn2Tienda.js

--- ExUn2Tienda.js
+++ ExUn2Tienda.js
@@ -2,50 +2,50 @@ var total = [];
 ////////////////
 // Aqui van las funciones //
 function AgreCarro1() {
-    var botonElement = document.getElementById("boton1");
-    var pElement = document.getElementById("uno");
-    var contador = 0;
-    botonElement.onclick = function () {
-        contador++;
-        pElement.textContent = contador;
-        var los3 = parseInt(document.getElementById('numero1').value);
-        total.push(los3);
+    var BotonElement = document.getElementById("boton1");
+    var PElement = document.getElementById("uno");
+    var Contador = 0;
+    BotonElement.onclick = function () {
+        Contador++;
+        PElement.textContent = Contador;
+        var Los3 = parseInt(document.getElementById('numero1').value);
+        total.push(Los3);
     }
 }
 
 function AgreCarro2() {
-    var botonElement = document.getElementById("boton2");
-    var pElement = document.getElementById("uno");
-    var contador = 0;
-    botonElement.onclick = function () {
-        contador++;
-        pElement.textContent = contador;
-        var los3 = parseInt(document.getElementById('numero2').value);
-        total.push(los3);
+    var BotonElement = document.getElementById("boton2");
+    var PElement = document.getElementById("uno");
+    var Contador = 0;
+    BotonElement.onclick = function () {
+        Contador++;
+        PElement.textContent = Contador;
+        var Los3 = parseInt(document.getElementById('numero2').value);
+        total.push(Los3);
     }
 }
 
 function AgreCarro3() {
-    var botonElement = document.getElementById("boton3");
-    var pElement = document.getElementById("dos");
-    var contador = 0;
-    botonElement.onclick = function () {
-        contador++;
-        pElement.textContent = contador;
-        var los3 = parseInt(document.getElementById('numero3').value);
-        total.push(los3);
+    var BotonElement = document.getElementById("boton3");
+    var PElement = document.getElementById("Dos");
+    var Contador = 0;
+    BotonElement.onclick = function () {
+        Contador++;
+        PElement.textContent = Contador;
+        var Los3 = parseInt(document.getElementById('numero3').value);
+        total.push(Los3);
     }
 }
 
 function AgreCarro4() {
-    var botonElement = document.getElementById("boton4");
-    var pElement = document.getElementById("dos");
-    var contador = 0;
-    botonElement.onclick = function () {
-        contador++;
-        pElement.textContent = contador;
-        var los3 = parseInt(document.getElementById('numero4').value);
-        total.push(los3);
+    var BotonElement = document.getElementById("boton4");
+    var PElement = document.getElementById("dos");
+    var Contador = 0;
+    BotonElement.onclick = function () {
+        Contador++;
+        PElement.textContent = Contador;
+        var Los3 = parseInt(document.getElementById('numero4').value);
+        total.push(Los3);
     }
 }
 
@@ -53,13 +53,13 @@ function Calcular() {
     let uno = document.getElementById("uno");
     let monitorValue = parseInt(uno.innerHTML);
 
-    let dos = document.getElementById("dos");
-    var dosValue = parseInt(dos.innerHTML);
+    let Dos = document.getElementById("Dos");
+    var DosValue = parseInt(Dos.innerHTML);
     
-    let sum = 0;
+    let Sum = 0;
 
     for (let index = 0; index < total.length; index++) {
-        sum += total[index];
+        Sum += total[index];
     }
-    document.getElementById("resultado").value = sum;
+    document.getElementById("resultado").value = Sum;
 }
